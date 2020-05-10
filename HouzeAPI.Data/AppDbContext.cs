64d84db845022d2001297879f6f6328a2f0e3b34
ColossalBoss <ogbaragodwin@Ogbaras-MacBook-Pro.local>
2020-05-10 05:30:25 +0100

    Update code

--- HouzeAPI.Data/AppDbContext.cs
+++ HouzeAPI.Data/AppDbContext.cs
@@ -6,11 +6,11 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace HouzeAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<AppUser>, IDesignTimeDbContextFactory<AppDbContext>
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext()
         {
-
+            //, IDesignTimeDbContextFactory<AppDbContext>
         }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
@@ -28,13 +28,13 @@ namespace HouzeAPI.Data
         //    throw new NotImplementedException();
         //}
 
-        public AppDbContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            //optionsBuilder.UseMySql("Server=localhost;DataBase.....");
-            //optionsBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=username;Password=secret;Database=todos;");
+        //public AppDbContext CreateDbContext(string[] args)
+        //{
+        //    var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        //    //optionsBuilder.UseMySql("Server=localhost;DataBase.....");
+        //    //optionsBuilder.UseNpgsql("Server=localhost;Port=5432;User Id=username;Password=secret;Database=todos;");
 
-            return new AppDbContext(optionsBuilder.Options);
-        }
+        //    return new AppDbContext(optionsBuilder.Options);
+        //}
     }
 }
