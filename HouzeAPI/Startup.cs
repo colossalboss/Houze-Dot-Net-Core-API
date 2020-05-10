18e78100868d4b35c7982d5c0a9ee631fff391d0
ColossalBoss <ogbaragodwin@Ogbaras-MacBook-Pro.local>
2020-05-10 17:21:15 +0100

    Update code

--- HouzeAPI/Startup.cs
+++ HouzeAPI/Startup.cs
@@ -59,19 +59,19 @@ namespace HouzeAPI
                 else
                 {
                     // Use connection string provided at runtime by Heroku.
-                    //var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-                    connStr = "Server=localhost;Port=5432;User Id=username;Password=secret;Database=todos;";
-                    // Parse connection URL to connection string for Npgsql
-                    //connUrl = connUrl.Replace("postgres://", string.Empty);
-                    //var pgUserPass = connUrl.Split("@")[0];
-                    //var pgHostPortDb = connUrl.Split("@")[1];
-                    //var pgHostPort = pgHostPortDb.Split("/")[0];
-                    //var pgDb = pgHostPortDb.Split("/")[1];
-                    //var pgUser = pgUserPass.Split(":")[0];
-                    //var pgPass = pgUserPass.Split(":")[1];
-                    //var pgHost = pgHostPort.Split(":")[0];
-                    //var pgPort = pgHostPort.Split(":")[1];
-                    //connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    //connStr = "Server=localhost;Port=5432;User Id=username;Password=secret;Database=todos;";
+                    //Parse connection URL to connection string for Npgsql
+                   connUrl = connUrl.Replace("postgres://", string.Empty);
+                   var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
                 }
                 // Whether the connection string came from the local development configuration file
                 // or from the environment variable from Heroku, use it to set up your DbContext.
